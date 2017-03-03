c5815361dc753feee62f90f873826e304ff4cab2
neuphys-danielrose <drose@cbs.mpg.de>
2017-03-03 21:29:14 +0100

    Wrote up installation steps.

--- nipype/interfaces/cbstools/__init__.py
+++ nipype/interfaces/cbstools/__init__.py
@@ -2,3 +2,4 @@ from .intensitypy import IntensityBackgroundEstimator
 
 # TODO: change reference to cbstoolsjcc
 # TODO: test whether cbstools is installed into the python path and show error if not.
+
